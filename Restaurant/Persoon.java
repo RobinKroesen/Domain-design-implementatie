a82a81cfe09edc0d6f9d0f7d3576d61b17896d10
RobinKroesen <74556141+RobinKroesen@users.noreply.github.com>
2021-04-22 16:32:20 +0200

    Implementatie van persoon

---
+++ Restaurant/Persoon.java
@@ -0,0 +1,14 @@
+package Restaurant;
+
+public class Persoon {
+
+    protected java.lang.String naam;
+
+    public void setNaam(java.lang.String naam) {
+        this.naam = naam;
+    }
+
+    public java.lang.String getNaam() {
+        return naam;
+    }
+}
